550ac70df82300e942b9daaaa2ffea8b7b73e0c8
RobertaBGH <roberta.hauser@outlook.com>
2022-07-20 15:53:10 -0300

    funcionario/models/funcao

--- funcionario/models.py
+++ funcionario/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class funcao(models.Model):
+    cargo=models.CharField(verbose_name="Função",max_length=194,unique=True)
+
+    class Meta:
+        verbose_name="funcao"
+        verbose_name_plural="funcoes"
+        db_table="tb_funcao"
+
+    def __str__(self):
+        return self.funcao
